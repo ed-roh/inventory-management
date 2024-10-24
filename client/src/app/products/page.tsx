b8582b1a5c18392793aa980ddb9e8e17b855b9ea
Aniket0898 <aniketpatil8451@gmail.com>
2024-10-25 02:17:07 +0530

    s3 urls updated

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -81,7 +81,7 @@ const Products = () => {
             >
               <div className="flex flex-col items-center">
                 <Image
-                  src={`https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/product${
+                  src={`https://s3-inventory-management-demo.s3.us-east-2.amazonaws.com/product${
                     Math.floor(Math.random() * 3) + 1
                   }.png`}
                   alt={product.name}
